c865f6a5ce6318f6e8e2348c7965b7fe55d1c820
the bao <lethebao2507@gmail.com>
2024-09-10 12:16:01 +0700

    config theme mui

--- eslint.config.js
+++ eslint.config.js
@@ -1,81 +1,68 @@
-import js from '@eslint/js'
 import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginJs from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
 
 export default [
-  { ignores: ['dist'] },
   {
-    env: { browser: true, es2020: true, node: true },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:react/jsx-runtime',
-      'plugin:react-hooks/recommended'
-    ],
-  },
-  {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.js', '**/*.jsx'], // Hỗ trợ cả .js và .jsx
     languageOptions: {
-      ecmaVersion: 2020,
+      sourceType: 'module',
       globals: globals.browser,
+      ecmaVersion: 'latest',
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        ecmaFeatures: {
+          jsx: true // Hỗ trợ JSX
+        }
+      }
     },
-    settings: { react: { version: '18.3' } },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: pluginReact
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      // React
-      'react-refresh/only-export-components': 'warn',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'react/prop-types': 0,
-      'react/display-name': 0,
-
-      // MUI
-      'no-restricted-imports': [
-        'error',
-        {
-          'patterns': ['@mui/*/*/*']
-        }
-      ],
-
-      //Comon
+      'react/jsx-uses-react': 'off', // Không cần import React từ React 17+
+      'react/jsx-uses-vars': 'warn',
+      // Cảnh báo khi có bất kỳ console.log hoặc các lệnh console khác
       'no-console': 1,
+      // Cảnh báo khi có cấu trúc `if` không cần thiết hoặc cô đơn
       'no-lonely-if': 1,
+      // Cảnh báo khi có biến được khai báo nhưng không được sử dụng
       'no-unused-vars': 1,
+      // Cảnh báo khi có khoảng trắng không cần thiết ở cuối dòng
       'no-trailing-spaces': 1,
+      // Cảnh báo khi có nhiều khoảng trắng giữa các ký tự
       'no-multi-spaces': 1,
+      // Cảnh báo khi có nhiều dòng trống liên tiếp
       'no-multiple-empty-lines': 1,
+      // Đặt quy tắc để có một khoảng trắng trước khối mã (block)
       'space-before-blocks': ['error', 'always'],
+      // Cảnh báo khi không có khoảng trắng bên trong dấu ngoặc của đối tượng
       'object-curly-spacing': [1, 'always'],
+      // Cảnh báo khi thụt lề không đúng (2 khoảng trắng)
       'indent': ['warn', 2],
+      // Cảnh báo khi có dấu chấm phẩy thừa ở cuối câu lệnh (không sử dụng dấu chấm phẩy)
       'semi': [1, 'never'],
+      // Đặt quy tắc để sử dụng dấu nháy đơn cho chuỗi (string) thay vì dấu nháy kép
       'quotes': ['error', 'single'],
+      // Cảnh báo khi không có khoảng trắng bên trong dấu ngoặc của mảng
       'array-bracket-spacing': 1,
+      // Tắt quy tắc kiểm tra kiểu dòng mới (linebreak) (có thể cần cấu hình nếu không phù hợp)
       'linebreak-style': 0,
+      // Cảnh báo khi có cấu trúc mã không mong đợi hoặc có thể gây nhầm lẫn
       'no-unexpected-multiline': 'warn',
+      // Cảnh báo khi không có khoảng trắng sau từ khóa như `if`, `for`, `while`, v.v.
       'keyword-spacing': 1,
+      // Cảnh báo khi có dấu phẩy dư thừa ở cuối danh sách các phần tử trong mảng hoặc đối tượng
       'comma-dangle': 1,
+      // Cảnh báo khi có khoảng trắng không đúng sau dấu phẩy
       'comma-spacing': 1,
+      // Cảnh báo khi không có khoảng trắng xung quanh các dấu mũi tên trong hàm mũi tên (arrow functions)
       'arrow-spacing': 1
     },
+    settings: {
+      react: {
+        version: 'detect' // Tự động phát hiện phiên bản React
+      }
+    }
   },
+  pluginJs.configs.recommended
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
 import Button from '@mui/material/Button'
-import AcUnitIcon from '@mui/icons-material/AcUnit';
+import AcUnitIcon from '@mui/icons-material/AcUnit'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
+      <a href="" target='blank'></a>
+      <Typography variant='body1' color='text.secondary'>Test Typography</Typography>
       <div>thebaodev</div>
-      <Button variant='contained'>Hello word</Button>
+      <Button variant='contained' color='success'>Hello word</Button>
       <Button variant='outlined'>Hi all</Button>
       <AcUnitIcon />
     </>
